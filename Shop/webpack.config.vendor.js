153303391e94172675c184ef168f11aeabb224fa
QVM0161195 <quanvo.dev@gmail.com>
2018-03-26 16:40:46 +0700

    reup source code

--- Shop/webpack.config.vendor.js
+++ Shop/webpack.config.vendor.js
@@ -15,14 +15,10 @@ const treeShakableModules = [
     'zone.js',
 ];
 const nonTreeShakableModules = [
-    'bootstrap',
-    'bootstrap/dist/css/bootstrap.css',
     'es6-promise',
     'es6-shim',
     'event-source-polyfill',
-    'jquery',
-    'font-awesome/css/font-awesome.css',
-    'bootstrap/dist/js/bootstrap.js'
+    'jquery'
 ];
 const allModules = treeShakableModules.concat(nonTreeShakableModules);
 module.exports = (env) => {
